f1eeb82273ad5181c5530526b15d821ec1b857fa
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2025-10-03 20:14:53 -0400

    .

---
+++ Day-05/mod-03/Demonstration C.sql
@@ -0,0 +1,39 @@
+-- Demonstration C - Use the AdventureWorksLT database on SQL Azure
+
+-- Step 1: Connect to the AdventureWorksLT database
+-- The USE statement is not currently compatible with SQL Azure databases.
+-- Instead, select AdventureWorksLT from the Available Databases list
+
+
+-- Step 2: Column Aliases
+-- Select and execute the following query to show the use of column aliases
+-- in the SELECT clause
+SELECT CustomerID AS CustomerNo, Title, FirstName, LastName AS Surname
+FROM salesLT.Customer
+
+SELECT CustomerID CustomerNo, Title, FirstName, LastName AS Surname
+FROM salesLT.Customer
+
+-- Step 3: Column Aliases
+-- Select and execute the following query to show the use of column aliases
+-- to label calculated columns in the SELECT clause
+SELECT ProductID, Name, ListPrice, (ListPrice * 1.13) AS MarkUp
+FROM SalesLT.Product;
+
+
+-- Step 4: Column Aliases
+-- Select and execute the following query to show the use of column aliases
+-- to label calculated columns in the SELECT clause
+SELECT SalesOrderNumber, 
+		PurchaseOrderNumber, 
+		CustomerID, 
+		OrderDate,
+		YEAR(OrderDate) AS YearPurchased 
+FROM SalesLT.SalesOrderHeader
+
+
+
+-- Step 5: Table Aliases
+-- Select and execute the following query to show the use of table aliases
+SELECT P.ProductNumber, P.Name, P.Color
+FROM SalesLT.Product AS P;
