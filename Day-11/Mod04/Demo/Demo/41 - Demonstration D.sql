a468131c8229421e7bcfa570812f7be5c09bcdc7
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2025-10-20 20:06:04 -0400

    .

--- Day-11/Mod04/Demo/Demo/41 - Demonstration D.sql
+++ Day-11/Mod04/Demo/Demo/41 - Demonstration D.sql
@@ -8,6 +8,71 @@ GO
 -- Select and execute the following query
 -- to display all employees with managers
 -- and the manager's ID and name.
+
+
+select * from hr.Employees e
+
+select * into hr.managers
+from (
+select e.empid as manager_id , 
+concat(e.firstname , ' ', e.lastname) as Manager_name
+from hr.employees e
+where e.empid in (select distinct e.mgrid  from hr.Employees e	where mgrid is not null )
+) as A
+
+select * from hr.Employees e
+select * from hr.managers
+
+select e.firstname + ' ' + e.lastname as EmployeeName ,
+		m.Manager_name	
+from 
+	hr.Employees e
+	inner join 
+	hr.managers m
+on e.mgrid = m.manager_id
+
+
+
+select e.firstname + ' ' + e.lastname as EmployeeName ,
+		m.Manager_name	
+from 
+	hr.Employees e
+	inner join 
+	hr.managers m
+on e.mgrid = m.manager_id
+
+
+select e.firstname + ' ' + e.lastname as EmployeeName ,
+		m.Manager_name	
+from 
+	hr.Employees e
+	left outer join 
+	hr.managers m
+on e.mgrid = m.manager_id
+
+Drop table hr.managers
+
+
+
+
+select 
+e.firstname + ' ' + e.lastname as EmployeeName ,
+m.firstname + ' ' + m.lastname as ManagerName 
+from hr.Employees as E
+inner join hr.Employees as M
+on e.mgrid = m.empid
+
+
+
+select 
+e.firstname + ' ' + e.lastname as EmployeeName ,
+m.firstname + ' ' + m.lastname as ManagerName 
+from hr.Employees as E
+left outer join hr.Employees as M
+on e.mgrid = m.empid
+
+
+
  SELECT e.empid ,e.lastname as empname,e.title,e.mgrid, m.lastname as mgrname
   FROM HR.Employees AS e
   JOIN HR.Employees AS m
